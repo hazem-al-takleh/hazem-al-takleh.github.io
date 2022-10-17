02a2977d4454c491894854000b053eaf0788a3f8
ASUS <hazem.thecla.ht@gmail.com>
2022-10-18 01:40:50 +0300

    hero and final smudges

--- script/script.js
+++ script/script.js
@@ -3,10 +3,12 @@ const header = document.querySelector('.navbar');
 console.log(header)
 window.onscroll = function() {
         const top = window.scrollY;
-        if (top >= 100) {
+        if (top >= 400) {
             header.classList.add('navbarDark');
+            header.classList.add('shadow');
         } else {
             header.classList.remove('navbarDark');
+            header.classList.remove('shadow');
         }
     }
     // collapse navbar after click on small devices
@@ -21,7 +23,20 @@ function sendEmail() {
     var name = document.getElementById('name').value
     var email = document.getElementById('email').value
     var subject = document.getElementById('subject').value
-    var body = document.getElementById('body').value
-    window.open('mailto:hazem.thecla.ht@gmail.com');
-
+    var emailBody = document.getElementById('body').value
+    var myEmail = 'hazem.thecla.ht@gmail.com'
+    var smtpEmail = 'hamzeh.tamzeeh@gmail.com'
+    var smtpPass = '123456789aaaa@@@@'
+    Email.send({
+            Host: "smtp.gmail.com",
+            Username: smtpEmail,
+            Password: smtpPass,
+            To: myEmail,
+            From: email,
+            Subject: subject,
+            Body: emailBody,
+        })
+        .then(function(message) {
+            alert("Mail has been sent successfully")
+        });
 }
